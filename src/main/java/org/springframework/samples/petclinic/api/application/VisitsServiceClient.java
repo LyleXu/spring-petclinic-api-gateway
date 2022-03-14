c07331eb4a51563dc53afa2de2f2a9732dd0badb
luxu-ms <92287281+luxu-ms@users.noreply.github.com>
2022-03-14 17:01:20 +0800

    use private endpoint for TAP

--- src/main/java/org/springframework/samples/petclinic/api/application/VisitsServiceClient.java
+++ src/main/java/org/springframework/samples/petclinic/api/application/VisitsServiceClient.java
@@ -33,7 +33,7 @@ import static java.util.stream.Collectors.joining;
 public class VisitsServiceClient {
 
     // Could be changed for testing purpose
-    private String hostname = "http://visits-service/";
+    private String hostname = "http://visits-service-00001-private/";
 
     private final WebClient.Builder webClientBuilder;
 
