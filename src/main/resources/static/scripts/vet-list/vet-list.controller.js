5638653d36cd9e9c030d24f8fb3cce0a28e20f54
luxu-ms <92287281+luxu-ms@users.noreply.github.com>
2022-03-31 23:29:19 +0800

    just test url

--- src/main/resources/static/scripts/vet-list/vet-list.controller.js
+++ src/main/resources/static/scripts/vet-list/vet-list.controller.js
@@ -4,7 +4,7 @@ angular.module('vetList')
     .controller('VetListController', ['$http', function ($http) {
         var self = this;
 
-        $http.get('api/vets-service/vets').then(function (resp) {
+        $http.get('https://ascpetclinicentdev1-gateway-07451.svc.azuremicroservices.io/api/vets-service/vets').then(function (resp) {
             self.vetList = resp.data;
         });
     }]);
